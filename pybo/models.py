bd8650da26a81b9d4a6670232628b5969a894532
ImYoungBin <sys02115@naver.com>
2023-02-15 18:25:25 +0900

    add login

--- pybo/models.py
+++ pybo/models.py
@@ -16,6 +16,6 @@ class Answer(db.Model):
 
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    user_name = db.Column(db.String(150), unique=True, nullable=False)
+    username = db.Column(db.String(150), unique=True, nullable=False)
     password = db.Column(db.String(200), nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
